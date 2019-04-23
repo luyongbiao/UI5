8ee6bd9125dce32a981a7d67cbdf9f6eff763429
biao <1565442611@qq.com>
2019-04-23 14:05:40 +0800

    Initial commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function(Controller) {
+    "use strict";
+
+    return Controller.extend("UI5.controller.Main", {
+        onInit: function(oEvent){
+            sap.ui.model
+        }
+    });
+});
